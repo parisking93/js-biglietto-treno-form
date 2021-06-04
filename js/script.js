fad15cdc3ebd10b562e58156e3fe90a870d292b4
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-04 17:36:26 +0200

    add controllo input

--- js/script.js
+++ js/script.js
@@ -3,33 +3,42 @@ var genera = document.getElementById('genera');
 // call back function click genera
 genera.addEventListener('click', function(){
     var nome = document.getElementById('nome').value;
-    var km = document.getElementById('km').value;
+    nome = nome.charAt(0).toUpperCase() + nome.slice(1);
+    var km = parseFloat(document.getElementById('km').value);
+    console.log(typeof km);
     var eta = document.getElementById('eta').value;
+    eta = eta.charAt(0).toUpperCase() + eta.slice(1);
     var codiceRandom = Math.floor(Math.random() * (100000 - 90000) + 90000);
     var carrozzaRandom = Math.floor(Math.random() * 9) + 1 ;
     var prezzoCorsa = km * 0.21;
 
     // controllo se c'è lo sconto 
-    if (eta == 'over 65') {
+    if (eta == 'Over 65') {
         prezzoCorsa *= 0.6;
-    } else if (eta == 'minorenne') {
+    } else if (eta == 'Minorenne') {
         prezzoCorsa *= 0.8;
     } else {
-        eta = 'nessuno';
+        eta = 'Nessuno';
+    }
+    if (nome !== "" && !(isNaN(km))) {
+        // output
+        document.getElementById("invisibile").className = "block"; 
+        document.getElementById('nome-pass').innerHTML = nome;
+        document.getElementById('sconto').innerHTML = 'Sconto ' + '<br>' + eta;
+        document.getElementById('carrozza').innerHTML = carrozzaRandom;
+        document.getElementById('codice-cp').innerHTML = codiceRandom;
+        document.getElementById('costo').innerHTML = prezzoCorsa.toFixed(2) + ' €';
+
+    } else {
+        alert('inserisci i giusti dati nei campi o non lasciare campi vuoti');
     }
     
-    // output
-    document.getElementById("invisibile").className = "block"; 
-    document.getElementById('nome-pass').innerHTML = nome;
-    document.getElementById('sconto').innerHTML = 'sconto ' + '<br>' + eta;
-    document.getElementById('carrozza').innerHTML = carrozzaRandom;
-    document.getElementById('codice-cp').innerHTML = codiceRandom;
-    document.getElementById('costo').innerHTML = prezzoCorsa.toFixed(2) + ' €';
 });
 
 var annulla = document.getElementById('annulla');
 
 annulla.addEventListener('click', function(){
+    // azzero tutto 
     nome.value = "";
     km.value= "";
     document.getElementById("invisibile").className = "none"; 
